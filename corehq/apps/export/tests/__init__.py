6750e3ff20960d1f486145ef9b7510de8344228a
NoahCarnahan <ncarnahan@dimagi.com>
2016-01-26 09:47:17 -0500

    [WIP] Rough outline of sql get_export_file() implementation

--- corehq/apps/export/tests/__init__.py
+++ corehq/apps/export/tests/__init__.py
@@ -1,3 +1,4 @@
 from corehq.apps.export.tests.test_form_schema import *
 from corehq.apps.export.tests.test_table_configuration import *
 from corehq.apps.export.tests.test_export_data_schema import *
+from corehq.apps.export.tests.test_get_export_file import *
